978587283c654e616ce3df24fc4b33ffcfde77ca
Mewerton Melo <mewerton@gmail.com>
2022-08-10 10:19:48 -0300

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,8 +4,23 @@ const UsersController = require("../controllers/UsersController")
 
 const usersRoutes = Router()
 
+
 const usersController = new UsersController()
 
 usersRoutes.post("/", usersController.create) 
 
 module.exports = usersRoutes
+
+
+// Criando Middleware 
+/*
+function myMiddleware(request, response, next){
+ console.log("você passou pelo Middleware")
+ 
+ if(!request.body.isAdmin){
+    return response.json({message: "user unauthorized"})
+ }
+
+ next()
+}
+*/
