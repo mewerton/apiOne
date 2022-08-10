9efca0bcbcd7d29ec46d83976f2da5cb32f1b54a
Mewerton Melo <mewerton@gmail.com>
2022-08-10 10:19:12 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,35 @@
 //const { response } = require("express");
 //const { request, response } = require("express");
-const express = require("express");
+
+require("express-async-errors")
+
+const AppError = require("./utils/AppError")
+
+const { request, response } = require("express")
+const express = require("express")
 
 const routes = require("./routes")
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 app.use(routes)
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    console.error(error)
+
+    return response.status(500).json({
+        status: "error",
+        message: "internal server error"
+    })
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`server ir running on Port ${PORT}`));
