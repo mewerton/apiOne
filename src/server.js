acf343474be5711b77e997719d945ea27636fcfe
Mewerton Melo <mewerton@gmail.com>
2022-09-03 16:04:10 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -34,6 +34,7 @@ app.use((error, request, response, next) => {
 const PORT = 3333;
 app.listen(PORT, () => console.log(`server ir running on Port ${PORT}`));
 
+
 /*
 app.get("/message/:id/:user",(request, response) => {
     const { id, user} = request.params;
